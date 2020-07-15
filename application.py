f3ac5f14208244f3bca9cac195b4f05aafaefa5e
musa <musauysal03@gmail.com>
2020-07-15 08:24:33 -0700

    girişte hata var

--- application.py
+++ application.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
+#confirm app
 app= Flask(__name__)
 app.secret_key = 'replace later'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
-    return render_template("index.html")
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great success!"
+
+    return render_template("index.html",form=reg_form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
