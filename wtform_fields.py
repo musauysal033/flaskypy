9599a46480e409def1aecea5aafb09748f68d910
musa <musauysal03@gmail.com>
2020-07-16 03:46:44 -0700

    girişte hata var- düzeltildi

--- wtform_fields.py
+++ wtform_fields.py
@@ -9,9 +9,11 @@ class RegistrationForm(FlaskForm):
     username = StringField('username_label',
         validators=[InputRequired(message="Username required"),
         Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+
     password = PasswordField('password_label',
         validators=[InputRequired(message="Password required"),
         Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
+
     confirm_pswd = PasswordField('confirm_pswd_label',
         validators=[InputRequired(message="Password required"),
         EqualTo('password',message="Passwords must match")])
