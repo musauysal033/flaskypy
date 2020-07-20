3df1d86af42f48436c6e6e6978fcf2fd5f3b4fd1
musa <musauysal03@gmail.com>
2020-07-20 03:55:16 -0700

    girilen kullanıcının veritabanında kontrol işlemleri yapıldı

--- application.py
+++ application.py
@@ -18,11 +18,6 @@ def index():
         username = reg_form.username.data
         password = reg_form.password.data
 
-        # Check username exists
-        user_object = User.query.filter_by(username=username).first()
-        if user_object:
-            return "Someone else has taken this username"
-
         # add user to db
         user =User(username=username, password=password)
         db.session.add(user)
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField
-from wtforms.validators import InputRequired,Length, EqualTo
+from wtforms.validators import InputRequired,Length, EqualTo, ValidationError
+
+from models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -18,3 +20,8 @@ class RegistrationForm(FlaskForm):
         validators=[InputRequired(message="Password required"),
         EqualTo('password',message="Passwords must match")])
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exits. Select a different username.")
