57467425896aa2173622dd49e011ab9f446710c7
musa <musauysal03@gmail.com>
2020-07-23 14:37:46 +0300

    giriş kimlik dogrulama işlemi eklendi

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect
-
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 from wtform_fields import *
 from models import *
@@ -11,6 +11,15 @@ app.secret_key = 'replace later'
 # Configure database
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://wackgzxnaxzann:ab7da9f7238d09c196ae6db9c3ce87a15f228d85accbfc453d2790b818d0c9c4@ec2-52-31-233-101.eu-west-1.compute.amazonaws.com:5432/de5tc62jsm4k4m'
 db = SQLAlchemy(app)
+
+# Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
@@ -38,7 +47,25 @@ def login():
 
     #Allow login  if validation success
     if login_form.validate_on_submit():
-        return "Logged in, finally!"
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('chat'))
+
+        return "Not logged in with :("
+
     return render_template("login.html",form =login_form)
+
+@app.route("/chat",methods=['GET','POST'])
+def chat():
+    if not current_user.is_authenticated:
+        return "Please login before accessing "
+
+    return "chat with me"
+
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    logout_user()
+    return "logged out using flask-login!"
 if __name__ == "__main__":
     app.run(debug=True)
