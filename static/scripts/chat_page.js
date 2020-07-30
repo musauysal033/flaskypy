270103355d000a8fde32a99965293a89d311edbe
musa <musauysal03@gmail.com>
2020-07-30 21:35:41 +0300

    deneme30

--- static/scripts/chat_page.js
+++ static/scripts/chat_page.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let msg = document.getElementById("user_message");
     msg.addEventListener("keyup", function(event) {
         event.preventDefault();
-        if (event.keyCode === 13) {
+        if (event.keyCode == 13) {
             document.getElementById("send_message").click();
         }
     });
