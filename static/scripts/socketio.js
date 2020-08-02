942017184da149e96dbd9210d4ef3a4b1e1e77b7
musa <musauysal03@gmail.com>
2020-08-02 17:03:09 +0300

    sendmessage7

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 // HTML to append
                 p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML
-                console.log(p.innerHTML)
+                console.log(p)
                 //Append
                 document.querySelector('#display-message-section').append(p);
             }
